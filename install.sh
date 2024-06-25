9e7b2a146f4f4e61fa4b7dbde59ff708f022f7e1
arshiacomplus <163334604+arshiacomplus@users.noreply.github.com>
2024-06-26 00:23:55 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,24 +20,31 @@ download_ip_scanner_config() {
 # Function to install or update a package
 install_or_update_package() {
     local package=$1
-    if ! command -v $package &> /dev/null
+    
+    if ! dpkg -s $package &> /dev/null
     then
         print_in_red "[*] $package is not installed. Installing it now..."
         pkg install $package -y
     else
-        print_in_red "[*] $package is already installed. Updating it now..."
-        pkg update $package -y
+        print_in_red "[*] $package is already installed. Checking for updates..."
+        pkg upgrade $package -y
     fi
 }
 
+# Clear the terminal before starting
+clear
+
 # Check if ping is installed and update if necessary
-install_or_update_package iputils
+install_or_update_package iputils-ping
 
 # Check if python is installed and update if necessary
 install_or_update_package python
 
 # Download ip_scanner_config.py
 download_ip_scanner_config
+
+# Clear the terminal after all installations and updates
+clear
 
 # Execute the ip_scanner_config.py script
 python ip_scanner_config.py
